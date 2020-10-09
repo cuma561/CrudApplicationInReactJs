bb303dd64f1b3e8b774354983ee76c66c468308d
Bartek-Krawczak <cuma561@github.com>
2020-10-09 21:30:36 +0200

    Add feature Company

---
+++ src/components/company/AddCompany.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import axios from 'axios';
+import { useHistory } from "react-router-dom";
+
+const AddCompany = () => {
+	let history = useHistory();
+	const[company,setCompany] = useState({
+		name: ""
+	});
+
+	const { name } = company;
+	const onInputChange = e => {
+    	setCompany({ ...company, [e.target.name]: e.target.value });
+  	};
+
+	return(
+		<div className="container">
+			<div className="w-75 mx-auto shadow p-5">
+				<h2 className="text-center mb-4">Add A Company</h2>
+				<form>
+					<div className="form-group">
+						<input 
+							type="text"
+							className="form-control form-control-lg"
+							placeholder="Enter Your Name"
+							name="name"
+              				value={name}
+              				onChange={e => onInputChange(e)}
+              			/>
+					</div>
+					<button className="btn btn-primary btn-block">Add Company</button>
+				</form>
+			</div>
+		</div>
+	)
+}
+export default AddCompany;
